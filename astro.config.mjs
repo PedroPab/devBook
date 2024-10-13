e3e81344645c78442824b71ac62d176eef28b7bf
Pedro Pablo <pmontesarroyave@gmail.com>
2024-10-13 15:04:34 -0500

    Actualizar dependencias de desarrollo y configuración de TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind()],
+
   env: {
     schema: {
       SHOW_BUY_BUTTON: envField.boolean({
@@ -16,5 +19,7 @@ export default defineConfig({
         default:'https://www.random.org/integers/?num=1&min=1&max=5&col=1&base=10&format=plain&rnd=new', context: 'server', access: 'public'
       }),
     }
-  }
+  },
+
+  adapter: vercel()
 })
